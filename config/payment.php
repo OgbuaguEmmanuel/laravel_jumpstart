da3e68fbd644c16c95bb72f9ac0f2a93c732e6ce
Emmanuel <eogbuagu@parkway.ng>
2025-08-02 03:21:15 +0100

    payment variables

---
+++ config/payment.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'driver' => env('PAYMENT_DRIVER', 'paystack'),
+    'minimums' => [
+        'NGN' => 50,
+        'USD' => 1,
+        'GBP' => 1,
+        // Add more currencies and values as needed
+    ],
+    'symbols' => [
+        'NGN' => '₦',
+        'USD' => '$',
+        'GBP' => '£',
+    ],
+    'paystack' => [
+        'baseurl' => env('PAYSTACK_BASE_URL', 'https://api.paystack.co'),
+        'secret' => env('PAYSTACK_SECRET_KEY'),
+        'callbackUrl' => env('PAYSTACK_CALLBACK_URL', config('app.url'))
+    ],
+];
