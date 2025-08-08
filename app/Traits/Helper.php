e652c08de398fb4aaae7d2fd7b0fbcee7ead2482
Emmanuel <eogbuagu@parkway.ng>
2025-08-08 09:38:32 +0100

    helper function for create notification

--- app/Traits/Helper.php
+++ app/Traits/Helper.php
@@ -2,7 +2,17 @@
 
 namespace App\Traits;
 
+use App\Models\Notification;
+
 trait Helper
 {
-
+    public function createNotification($notifiable, $type, $data)
+    {
+        return Notification::create([
+            'notifiable_id' => $notifiable->id,
+            'notifiable_type' => get_class($notifiable),
+            'type' => $type,
+            'data' => $data,
+        ]);
+    }
 }
