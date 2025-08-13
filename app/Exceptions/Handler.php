148449e9cac4b188eded8ba3446946dfbd7c27fe
Emmanuel <eogbuagu@parkway.ng>
2025-08-13 11:27:27 +0100

    clean up handler and add more exception handlers

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,23 +2,9 @@
 
 namespace App\Exceptions;
 
-use Throwable;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Lang;
-use Illuminate\Auth\AuthenticationException;
-use Illuminate\Validation\ValidationException;
-use Illuminate\Auth\Access\AuthorizationException;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use App\Helpers\APIExceptionHandler;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use MarcinOrlowski\ResponseBuilder\ResponseBuilder;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\HttpFoundation\Exception\BadRequestException;
-use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
-use Symfony\Component\Routing\Exception\RouteNotFoundException;
-
+use Throwable;
 class Handler extends ExceptionHandler
 {
     /**
@@ -44,110 +30,9 @@ class Handler extends ExceptionHandler
 
     public function render($request, Throwable $exception)
     {
-        if($request->expectsJson() || $request->routeIs('api.*')){
-            return $this->handleApiException($request, $exception);
+        if ($request->expectsJson() || $request->routeIs('api.*')) {
+            return (new APIExceptionHandler())->handle($request, $exception);
         }
 
-        return parent::render($request, $exception);
     }
-
-
-    public function handleApiException(Request $request, $exception){
-
-        if($exception instanceof MethodNotAllowedHttpException){
-            return ResponseBuilder::asError(Response::HTTP_METHOD_NOT_ALLOWED)
-                ->withHttpCode(Response::HTTP_METHOD_NOT_ALLOWED)
-                ->withMessage(strtoupper($request->getMethod()) . ' method is not allowed for this endpoint.')
-                ->build();
-        }
-
-        if($exception instanceof NotFoundHttpException){
-            return ResponseBuilder::asError(Response::HTTP_NOT_FOUND)
-                ->withHttpCode(Response::HTTP_NOT_FOUND)
-                ->withMessage($exception->getMessage())
-                ->build();
-        }
-
-        if($exception instanceof RouteNotFoundException){
-            return ResponseBuilder::asError(Response::HTTP_NOT_FOUND)
-                ->withHttpCode(Response::HTTP_NOT_FOUND)
-                ->withMessage($exception->getMessage())
-                ->build();
-        }
-
-        if($exception instanceof AuthorizationException){
-            return ResponseBuilder::asError(Response::HTTP_FORBIDDEN)
-                ->withHttpCode(Response::HTTP_FORBIDDEN)
-                ->withMessage($exception->getMessage())
-                ->build();
-        }
-
-        if($exception instanceof AuthenticationException){
-            return ResponseBuilder::asError(Response::HTTP_UNAUTHORIZED)
-                ->withHttpCode(Response::HTTP_UNAUTHORIZED)
-                ->withMessage('Log in to perform this action.')
-                ->build();
-        }
-
-        if($exception instanceof ModelNotFoundException){
-            return ResponseBuilder::asError(Response::HTTP_NOT_FOUND)
-                ->withHttpCode(Response::HTTP_NOT_FOUND)
-                ->withMessage('No query result was found for the resource.')
-                ->build();
-        }
-
-        if($exception instanceof ThrottleRequestsException){
-            return ResponseBuilder::asError(Response::HTTP_TOO_MANY_REQUESTS)
-                ->withHttpCode(Response::HTTP_TOO_MANY_REQUESTS)
-                ->withMessage(Lang::get('auth.throttle'))
-                ->build();
-        }
-
-        if($exception instanceof BadRequestException){
-            return ResponseBuilder::asError(Response::HTTP_BAD_REQUEST)
-                ->withHttpCode(Response::HTTP_BAD_REQUEST)
-                ->withMessage('Bad request.')
-                ->build();
-        }
-
-        if($exception instanceof ValidationException){
-            return ResponseBuilder::asError($exception->status)
-                ->withHttpCode($exception->status)
-                ->withData($exception->errors())
-                ->withMessage($exception->getMessage())
-                ->build();
-        }
-
-        if(method_exists($exception, 'getStatusCode')){
-            return ResponseBuilder::asError($exception->getStatusCode)
-                ->withHttpCode($exception->getStatusCode)
-                ->withMessage($exception->getMessage())
-                ->build();
-        }
-
-        if ($exception instanceof SocialAuthException) {
-            return ResponseBuilder::asError($exception->getCode() ?: Response::HTTP_BAD_REQUEST)
-                ->withHttpCode($exception->getCode() ?: Response::HTTP_BAD_REQUEST)
-                ->withMessage($exception->getMessage())
-                ->withData($exception->getContext())
-                ->build();
-        }
-
-        if ($exception instanceof HttpException) {
-            $safeMessage = app()->environment('production')
-                ? __('Something went wrong. Please try again.')
-                : $exception->getMessage(); // Show raw only in dev/test
-
-            return ResponseBuilder::asError($exception->getStatusCode())
-                ->withMessage($safeMessage)
-                ->build();
-
-        }
-
-        return ResponseBuilder::asError(Response::HTTP_INTERNAL_SERVER_ERROR)
-            ->withHttpCode(Response::HTTP_INTERNAL_SERVER_ERROR)
-            ->withMessage('A technical error occurred.')
-            ->build();
-    }
-
 }
--- app/Helpers/APIExceptionHandler.php
+++ app/Helpers/APIExceptionHandler.php
@@ -7,14 +7,19 @@ namespace App\Helpers;
 use App\Exceptions\SocialAuthException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Exceptions\PostTooLargeException;
 use Illuminate\Http\Exceptions\ThrottleRequestsException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 use MarcinOrlowski\ResponseBuilder\ResponseBuilder;
 use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
@@ -101,9 +106,9 @@ final class APIExceptionHandler
                 ->build();
         }
 
-        if (method_exists($throwable, 'getStatusCode')) {
-            return ResponseBuilder::asError($throwable->getStatusCode)
-                ->withHttpCode($throwable->getStatusCode)
+        if ($throwable instanceof AccessDeniedHttpException) {
+            return ResponseBuilder::asError(Response::HTTP_FORBIDDEN)
+                ->withHttpCode(Response::HTTP_FORBIDDEN)
                 ->withMessage($throwable->getMessage())
                 ->build();
         }
@@ -116,6 +121,51 @@ final class APIExceptionHandler
                 ->build();
         }
 
+        if ($throwable instanceof PostTooLargeException) {
+            return ResponseBuilder::asError(Response::HTTP_REQUEST_ENTITY_TOO_LARGE)
+                ->withHttpCode(Response::HTTP_REQUEST_ENTITY_TOO_LARGE)
+                ->withMessage('Uploaded file is too large.')
+                ->build();
+        }
+
+        if ($throwable instanceof QueryException) {
+            $message = app()->environment('production')
+                ? __('A database error occurred. Please try again later.')
+                : $throwable->getMessage();
+
+            return ResponseBuilder::asError(Response::HTTP_INTERNAL_SERVER_ERROR)
+                ->withHttpCode(Response::HTTP_INTERNAL_SERVER_ERROR)
+                ->withMessage($message)
+                ->build();
+        }
+
+        if ($throwable instanceof BindingResolutionException) {
+            $safeMessage = app()->environment('production')
+                ? __('Something went wrong while preparing your request. Please try again.')
+                : $throwable->getMessage();
+
+            return ResponseBuilder::asError(Response::HTTP_INTERNAL_SERVER_ERROR)
+                ->withHttpCode(Response::HTTP_INTERNAL_SERVER_ERROR)
+                ->withMessage($safeMessage)
+                ->build();
+        }
+
+        if ($throwable instanceof FileException) {
+            return ResponseBuilder::asError(Response::HTTP_BAD_REQUEST)
+                ->withHttpCode(Response::HTTP_BAD_REQUEST)
+                ->withMessage('There was an error processing your file.')
+                ->build();
+        }
+
+        if ($throwable instanceof \TypeError || $throwable instanceof \ErrorException) {
+            return ResponseBuilder::asError(Response::HTTP_INTERNAL_SERVER_ERROR)
+                ->withHttpCode(Response::HTTP_INTERNAL_SERVER_ERROR)
+                ->withMessage(app()->environment('production')
+                    ? 'Internal server error.'
+                    : $throwable->getMessage())
+                ->build();
+        }
+
         if ($throwable instanceof HttpException) {
             $safeMessage = app()->environment('production')
                 ? __('Something went wrong. Please try again.')
@@ -123,6 +173,13 @@ final class APIExceptionHandler
 
             return ResponseBuilder::asError($throwable->getStatusCode())
                 ->withMessage($safeMessage)
+                ->build();
+        }
+
+        if (method_exists($throwable, 'getStatusCode')) {
+            return ResponseBuilder::asError($throwable->getStatusCode)
+                ->withHttpCode($throwable->getStatusCode)
+                ->withMessage($throwable->getMessage())
                 ->build();
         }
 
