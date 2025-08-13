c31cea6a344d6bfeb3c600f589660fe6a3515ef6
Emmanuel <eogbuagu@parkway.ng>
2025-08-13 18:50:16 +0100

    create every user with user role unless it is created by admin

--- app/Http/Controllers/V1/Auth/RegisterController.php
+++ app/Http/Controllers/V1/Auth/RegisterController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\V1\Auth;
 
 use App\Actions\CreateUserAction;
+use App\Enums\RoleTypeEnum;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\CreateUserRequest;
 use MarcinOrlowski\ResponseBuilder\ResponseBuilder;
+use Spatie\Permission\Models\Role;
 
 class RegisterController extends Controller
 {
@@ -16,7 +18,10 @@ class RegisterController extends Controller
             $file = $request->file('profile_picture');
         }
 
-        $action->handle($request->validated(), $file);
+        $user = $action->handle($request->validated(), $file);
+
+        $role = Role::where('name', RoleTypeEnum::User)->get();
+        $user->syncRoles($role);
 
         return ResponseBuilder::asSuccess()
             ->withHttpCode(201)
